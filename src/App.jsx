c7e102297cb82523666587b81b72b63de8b06978
Lilia <lilia@student.dev-island.org>
2024-03-07 20:03:51 -1000

    implement user  navigation  with links to all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 import Students from "./Routes/Students/Students";
 import Cats from "./Routes/Cats/Cats";
 import Dogs from "./Routes/Dogs/Dogs";
 
 import "./App.css";
 
-
-
-
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Students />} />
-      <Route path="/cats" element={<Cats />} />
-      <Route path="/dogs" element={<Dogs />} />
-    </Routes>
-
-   </>
+      <nav className="navigation">
+        <ul className="nav-menu">
+          <li>
+            <NavLink to="/">Students Page</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Cats">Cats Page</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Dogs">Dogs Page</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Students />} />
+        <Route path="/cats" element={<Cats />} />
+        <Route path="/dogs" element={<Dogs />} />
+      </Routes>
+    </>
   );
 }
 
