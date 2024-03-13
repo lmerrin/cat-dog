c1acf8dfe3ea2fe11004bdf4edf4e58010d1a281
Lilia <lilia@student.dev-island.org>
2024-03-12 20:30:55 -1000

    minor change to Dog Details Page

--- src/Routes/DogDetailsPage.jsx
+++ src/Routes/DogDetailsPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { dogs } from '../data';
 import { useParams } from "react-router";
 
@@ -9,7 +8,7 @@ const dogId = Number(params.dogId);
 const singleDog = dogs.find((dog) => dog.id === dogId);
   return (
     <div className="page">
-    <h2>Cat Details</h2>
+    <h2>Dog Details</h2>
     <p>Name: {singleDog.name}</p>
     <p>Breed: {singleDog.breed}</p>
     <p>Age: {singleDog.age}</p>
